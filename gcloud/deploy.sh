a92eaa9c8272a47452060e6e4f420faf420ee983
Miss Cissy <hanirizo@gmail.com>
2017-11-27 22:29:36 +0700

    gcloud --quiet

--- gcloud/deploy.sh
+++ gcloud/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "nama project: [ENTER]:"
 read name
-gcloud app deploy ../public/app.yaml --project $name
+gcloud app deploy ../public/app.yaml --project $name --quiet
 gcloud app browse
