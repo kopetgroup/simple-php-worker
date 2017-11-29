da9db0681a5b22a8fc76233cc10f7933a59f15f4
Miss Cissy <hanirizo@gmail.com>
2017-11-30 01:38:22 +0700

    diresiki file filene

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 [![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
 
-# Slim-3 Framework / Simple
+Simple php worker
+
+#### Slim-3 Framework / Simple
 
 This is a simple skeleton project for Slim 3 that implements a simple REST API.
 Based on [akrabat's slim3-skeleton](https://github.com/akrabat/slim3-skeleton)
--- worker/run.php
+++ worker/run.php
@@ -9,46 +9,8 @@ $dotenv->load();
 $hostname = trim(shell_exec('hostname -I'));
 slack('bot initiate');
 
-//load redis
-if(isset($_SERVER['REDIS'])){
-  $r = $_SERVER['REDIS'];
-}else{
-  $r = '';
-}
-//$redis = new \Predis\Client($r);
-//looping
 while(true) {
 
-  /*
-  $e = $redis->keys('1*');
-  asort($e);
-  $e = array_values($e);
-  $job = $e[0];
-
-  $date = date('Y-m-d H:i:s');
-
-  //cek job
-  if(!empty($job)){
-
-    //get job
-    echo 'running job: "'.$job.'" @'.$date."\n";
-    slack('bot running: '.$job);
-
-    echo shell_exec('php $PWD/public/index.php '.$job);
-    //echo shell_exec('curl -sSXPATCH http://localhost:8080');
-    echo "\n";
-    //delete job
-    #$redis->lpop('job');
-    sleep(2);
-
-  }else{
-    //slack('bot entek ');
-    echo 'job entek. @'.$date."\n";
-    sleep(10);
-  }
-  */
-  //echo 'running job: "'.$job.'" -> '.$_SERVER['HTTP_HOST'].' @'.$date."\n";
-  $argv = $GLOBALS['argv'];
   slack('bot running: ->  '.$hostname.' / '.$job);
   sleep(4);
 
@@ -61,6 +23,6 @@ function slack($msg=''){
   shell_exec('curl -sSX POST \
     --data \'{"text":"'.$msg.' @'.date('Y-m-d H:i:s').'"}\' \
     -H \'Content-type: application/json\' \
-    \'https://hooks.slack.com/services/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\'');
+    \''.$_SERVER['SLACKBOT'].'\'');
 }
 exit;
